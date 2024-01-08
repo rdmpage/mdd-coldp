23f2d4c423558160e8fa7aeec0377a785cae6965
Roderic Page <rdmpage@gmail.com>
2024-01-08 18:07:15 +0000

    Add data

---
+++ sqlite.php
@@ -0,0 +1,105 @@
+<?php
+
+// SQLite functions
+
+//----------------------------------------------------------------------------------------
+// https://gist.github.com/fcingolani/5364532
+function import_csv_to_sqlite(&$pdo, $csv_path, $options = array())
+{
+    extract($options);
+
+    if (($csv_handle = fopen($csv_path, "r")) === FALSE)
+        throw new Exception('Cannot open CSV file');
+
+    if(!isset($delimiter))
+    {
+        $delimiter = ',';
+    }
+
+    if(!isset($table))
+    {
+        $table = preg_replace("/[^A-Z0-9]/i", '', basename($csv_path, '.csv'));
+	}
+ 
+    if(!isset($fields)){
+        $fields = array_map(function ($field){
+            return preg_replace("/[^A-Z0-9]/i", '', $field);
+        }, fgetcsv($csv_handle, 0, $delimiter));
+    }
+
+    $create_fields_str = join(', ', array_map(function ($field){
+        return "`$field` TEXT NULL";
+    }, $fields));
+
+    $pdo->beginTransaction();
+
+    $create_table_sql = "CREATE TABLE IF NOT EXISTS $table ($create_fields_str)";
+        
+    $pdo->exec($create_table_sql);
+
+	$insert_fields_str = join(', ', array_map(function ($field){
+        return "`$field`";
+    }, $fields));    
+    
+    $insert_values_str = join(', ', array_fill(0, count($fields),  '?'));
+    $insert_sql = "INSERT INTO $table ($insert_fields_str) VALUES ($insert_values_str)";
+    $insert_sth = $pdo->prepare($insert_sql);
+
+    $inserted_rows = 0;
+    while (($data = fgetcsv($csv_handle, 0, $delimiter)) !== FALSE) {
+    
+    	foreach ($data as $k => $v)
+    	{
+    		if ($v == "\\N")
+    		{
+    			$data[$k] = null;
+    		}    	
+    	}
+    	// print_r($data);
+    
+        $insert_sth->execute($data);
+        $inserted_rows++;
+    }
+
+    $pdo->commit();
+
+    fclose($csv_handle);
+
+    return array(
+            'table' => $table,
+            'fields' => $fields,
+            'insert' => $insert_sth,
+            'inserted_rows' => $inserted_rows
+        );
+
+}
+
+//----------------------------------------------------------------------------------------
+function do_query($pdo, $sql)
+{
+	$stmt = $pdo->query($sql);
+
+	$data = array();
+
+	while ($row = $stmt->fetch(\PDO::FETCH_ASSOC)) {
+
+		$item = new stdclass;
+		
+		$keys = array_keys($row);
+	
+		foreach ($keys as $k)
+		{
+			if ($row[$k] != '')
+			{
+				$item->{$k} = $row[$k];
+			}
+		}
+	
+		$data[] = $item;
+	}
+	
+	return $data;	
+}
+
+
+?>
